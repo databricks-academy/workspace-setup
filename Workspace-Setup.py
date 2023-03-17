47b3efadc8b182510b8a3fdaaa6a3024f6f85cce
SireInsectus <SireInsectus>
2023-03-17 03:07:58 +0000

    Updated dependencies

--- Workspace-Setup.py
+++ Workspace-Setup.py
@@ -24,7 +24,7 @@
 
 import requests
 
-version = spark.conf.get("dbacademy.library.version", "v3.0.48")
+version = spark.conf.get("dbacademy.library.version", "v3.0.49")
 
 try:
     from dbacademy import dbgems
