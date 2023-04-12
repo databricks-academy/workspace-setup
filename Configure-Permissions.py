130c4696d28539c53cb4076b319b8b251cead62e
SireInsectus <SireInsectus>
2023-04-12 21:51:52 +0000

    Removed unused notebook.

--- Configure-Permissions.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-# TODO for later, preheat the VMs in the classroom-setup script
-# This one operation is unique to the hive_metastore, giving users the ability to create user-scoped schemas/databases
-spark.sql("GRANT CREATE ON CATALOG hive_metastore TO users")
